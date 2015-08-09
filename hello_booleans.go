8a9b773df88664a8b7c296187c603f706175dc74
crazcalm <crazcalm@gmail.com>
2015-08-09 20:19:40 +0800

    Some booleans and if\else statements

---
+++ hello_booleans.go
@@ -0,0 +1,15 @@
+package main
+import(
+    "fmt"
+)
+
+//Note: If/else construction with booleans
+
+func main(){
+    bool1 := true
+    if bool1{
+        fmt.Printf("The value is true\n")
+    }else{
+        fmt.Printf("The value is false\n")
+    }
+}
