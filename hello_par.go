9b0b37f5277bd744ad97cf9b6b43076d360ec039
crazcalm <crazcalm@gmail.com>
2015-08-10 21:04:49 +0800

    a func that takes an undefined number of variables. Important

---
+++ hello_par.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main(){
+    x := Min(1, 3, 2, 0)
+    fmt.Printf("The minimum is: %d\n", x)
+    arr := []int{7, 9, 3, 5, 1}
+    x = Min(arr...)
+    fmt.Printf("The minimum in the array is: %d\n", x)
+}
+
+func Min(a ...int) int{
+    if len(a) == 0{
+        return 0
+    }
+
+    min := a[0]
+    for _, v := range a{
+        if v < min{
+            min = v
+        }
+    }
+    return min
+}
