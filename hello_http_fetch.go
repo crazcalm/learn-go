d481c91aed656458fd035dae448f24b0cfeda955
Crazcalm <Crazcalm@gmail.com>
2015-09-07 14:10:40 -0400

    How to get html pages

---
+++ hello_http_fetch.go
@@ -0,0 +1,22 @@
+package main
+
+import(
+	"fmt"
+	"net/http"
+	"io/ioutil"
+	"log"
+)
+
+func main(){
+	res, err := http.Get("http://www.google.com")
+	CheckError(err)
+	data, err := ioutil.ReadAll(res.Body)
+	CheckError(err)
+	fmt.Printf("Got: %q", string(data))
+}
+
+func CheckError(err error) {
+	if err != nil{
+		log.Fatal("Get: %v", err)
+	}
+}
