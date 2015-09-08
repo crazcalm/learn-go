8f97691c170b6a06e8a661aeaa27d1ff5c6467c7
Crazcalm <Crazcalm@gmail.com>
2015-09-08 03:16:05 -0400

    learning how to use cli tools

---
+++ hello_cli.go
@@ -0,0 +1,37 @@
+package main
+
+import(
+	"os"
+	"github.com/codegangsta/cli"
+)
+
+func main(){
+
+	author := cli.Author{"Marcus Willock", "crazcalm@gmail.com"}
+
+	app := cli.NewApp()
+	app.Name = "boom"
+	app.Usage = "make an explosive entrance"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name: "lang",
+			Value: "english",
+			Usage: "Language for the greeting",
+
+		},
+	}
+	app.Authors = []cli.Author{author}
+	app.Action = func(c *cli.Context){
+		name := "someone"
+		if len(c.Args()) > 0{
+			name = c.Args()[0]
+		}
+		if c.String("lang") == "spanish" {
+			println("Hola", name)
+		} else {
+			println("Hello", name)
+		}
+	}
+
+	app.Run(os.Args)
+}
