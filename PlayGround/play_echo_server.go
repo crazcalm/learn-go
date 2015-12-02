b296ccbf2f1bd9e3278f11a3e83393261e89c388
crazcalm <crazcalm@gmail.com>
2015-12-01 23:04:17 -0500

    The echo server works

---
+++ PlayGround/play_echo_server.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+)
+
+const (
+	CONN_HOST = ""
+	CONN_PORT = "3333"
+	CONN_TYPE = "tcp"
+)
+
+func main() {
+	// Listen of incoming connections
+	listen, err := net.Listen(CONN_TYPE, ":"+CONN_PORT)
+	if err != nil {
+		// log.Fatal makes an os call to exit the program after logging
+		// the issue
+		log.Fatal(err)
+	}
+	// Ensures that the connection is closed properly
+	defer listen.Close()
+
+	fmt.Println("Listening on: " + CONN_HOST + ":" + CONN_PORT)
+
+	// infinite loop to listen of incoming connections
+	for {
+		// listen of incoming connections
+		conn, err := listen.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// print info about incoming request
+		fmt.Printf("Received connection from: %s -> %s \n", conn.RemoteAddr(),
+			conn.LocalAddr())
+
+		// Handle Connection in goroutine
+		go HandleConn(conn)
+	}
+
+}
+
+func HandleConn(conn net.Conn) {
+	defer conn.Close()
+	input := bufio.NewScanner(conn)
+	for input.Scan() {
+		fmt.Fprintln(conn, "Received: "+input.Text())
+	}
+}
