bfef0209d0ba54de1032adf84d96a074aea38d50
crazcalm <crazcalm@gmail.com>
2015-12-02 12:31:08 -0500

    ran go fmt on it

--- PlayGround/play_user_input.go
+++ PlayGround/play_user_input.go
@@ -1,30 +1,29 @@
 package main
 
 import (
-    "fmt"
-    "bufio"
-    "os"
+	"bufio"
+	"fmt"
+	"os"
 )
 
-func main(){
-    var name string
+func main() {
+	var name string
 
-    input := bufio.NewScanner(os.Stdin)
+	input := bufio.NewScanner(os.Stdin)
 
-    fmt.Println("What is your name?")
+	fmt.Println("What is your name?")
 
+	for input.Scan() {
 
-    for input.Scan(){
+		name = input.Text()
+		if name != "" {
+			break
+		}
 
-        name = input.Text()
-        if name != ""{
-            break
-        }
+		fmt.Println("That name is no good. Try again.")
+	}
 
-        fmt.Println("That name is no good. Try again.")
-    }
-
-    // Note: ignoring potential errors from input.Err()
-    // Note: Don't know how to deal with those errors...
-    fmt.Printf("Hello %s\n", name)
+	// Note: ignoring potential errors from input.Err()
+	// Note: Don't know how to deal with those errors...
+	fmt.Printf("Hello %s\n", name)
 }
